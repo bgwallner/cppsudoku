7224c022015b5ca68375fcc5ac2e74f985ce5d83
Bo-Göran Wallner <bogoeran.wallner@gmail.com>
2024-02-17 21:46:20 +0100

    Initial solver

--- puzzle_solver.cpp
+++ puzzle_solver.cpp
@@ -7,3 +7,13 @@ puzzle_solver::PuzzleSolver::PuzzleSolver()
 puzzle_solver::PuzzleSolver::~PuzzleSolver()
 {
 }
+
+int puzzle_solver::PuzzleSolver::ForwardSolver(std::vector<std::vector<int>>& puzzle)
+{
+	return 0;
+}
+
+int puzzle_solver::PuzzleSolver::BackwardSolver(std::vector<std::vector<int>>& puzzle)
+{
+	return 0;
+}
--- puzzle_solver.h
+++ puzzle_solver.h
@@ -1,12 +1,20 @@
 #ifndef PUZZLE_SOLVER_H_
 #define PUZZLE_SOLVER_H_
 
+#include <vector>
+
 namespace puzzle_solver {
 
 class PuzzleSolver {
 public:
 	PuzzleSolver();
 	~PuzzleSolver();
+
+	// Solve puzzle using DFS by evaluating values 1->9
+	int ForwardSolver(std::vector<std::vector<int>>& puzzle);
+
+	// Solve puzzle using DFS by evaluating values 9->1
+	int BackwardSolver(std::vector<std::vector<int>>& puzzle);
 
 };
 
