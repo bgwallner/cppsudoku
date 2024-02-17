76776346aa438ba6a006264e3933faf787eaebc9
Bo-Göran Wallner <bogoeran.wallner@gmail.com>
2024-02-17 22:10:04 +0100

    Getting first free element.

--- puzzle_solver.cpp
+++ puzzle_solver.cpp
@@ -2,10 +2,31 @@
 
 namespace puzzle_solver
 {
+
+constexpr int kDim{ 9u };
+constexpr int kMaxElems{ 81u };
+constexpr int kMaxVal{ 9u };
+constexpr int kOK{ 0 };
+constexpr int kNotOK{ -1 };
+
 int PuzzleSolver::GetFirstFreeElement(const std::vector<std::vector<int>>& puzzle,
 	int& row, int& col)
 {
-	return 0;
+    // Find first element having value = 0. If none found puzzle is solved.
+    for (int row_{ 0 }; row_ < kDim; row_++)
+    {
+        for (int col_{ 0 }; col_ < kDim; col_++)
+        {
+            if (0 == puzzle[row_][col_])
+            {
+                /* Found available element */
+                row = row_;
+                col = col_;
+                return kOK;
+            }
+        }
+    }
+    return kNotOK;
 }
 
 PuzzleSolver::PuzzleSolver()
